6c38760f27790f26c9dd3be54137927921c7f5dd
Thiago Pereira da Silva <thiagosilva.inf@gmail.com>
2019-11-25 16:27:14 -0300

    commit inicial

---
+++ client_json.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+import numpy as np
+from PIL import Image
+import requests
+import json
+
+addr = 'http://localhost:5000'
+URL = addr + '/test'
+image_name = 'image.jpg'
+
+# prepare headers for http request
+content_type = 'application/json'
+headers = {'content-type': content_type,
+           'label': 'myLabel.jpg',
+           'description': 'myDescription',
+           'date': 'myDate',
+           'calibration': 'myCalibration',
+           'author': 'myAuthor'}
+
+image = Image.open(image_name)
+json_data = json.dumps(np.array(image).tolist())
+data = {'image':json_data}
+#print (json.dumps(json_data))
+
+response = requests.post(URL, data=data, headers=headers)
+print(json.loads(response.text))
---
+++ server.py
@@ -0,0 +1,132 @@
+from flask import Flask, request, Response
+from PIL import Image
+from zipfile import ZipFile
+import numpy as np
+import sys
+import getopt
+import glob
+import piexif
+import os
+import werkzeug
+import jsonpickle
+import cv2
+
+
+def procurar_imagens(pasta_fonte='./download'):
+    #arquivos = os.listdir(pasta_fonte)
+    arquivos = glob.glob(os.path.join(pasta_fonte, '*.jpg'))
+
+    if arquivos:
+        for arquivo in arquivos:
+            if (arquivo.endswith(".jpg") != True):
+                arquivos.remove(arquivo)
+    else:
+        print('[Alerta]\tNenhum arquivo JPEG encontrado em "{0}"!'.format(
+            pasta_fonte))
+    return arquivos
+
+
+def corrigir_imagens(imagens, pasta_destino=''):
+    for imagem in imagens:
+        try:
+            img = Image.open(imagem)
+        except Exception:
+            print('[Erro]\t\tImpossivel ler o arquivo "{0}"'.format(imagem))
+            sys.exit(2)
+
+        exif_dict = piexif.load(imagem)
+        #exif_dict = piexif.load(img.info["exif"])
+        # exif_dict = img._getexif() //nao funciona
+        print('[Info]\t\tEXIF')
+        print(exif_dict)
+
+        arquivo = os.path.basename(imagem)
+
+        try:
+            print('[Info]\t\tAdding EXIF in "{0}"'.
+                  format(arquivo))
+            exif_dict['Exif'][piexif.ExifIFD.FNumber] = (71, 10)
+        except Exception:
+            print('[Erro]\t\tError ao tentar adicionar informacao no arquivo "{0}"'.
+                  format(imagem))
+            sys.exit(2)
+
+        exif_bytes = piexif.dump(exif_dict)
+
+        if not os.path.exists(pasta_destino):
+            os.mkdir(pasta_destino)
+
+        nova_imagem = os.path.join(pasta_destino, arquivo)
+
+        print('[Info]\t\tSalving file "{0}"'.
+              format(nova_imagem))
+
+        img.save(nova_imagem, "jpeg", exif=exif_bytes)
+
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+""" @app.route('/test', methods=['POST'])
+def test():
+    print("Received a image inside a json message")
+    print()
+    resp = {'message': 'Ok'
+            }
+    response_pickled = jsonpickle.encode(resp)
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+ """
+
+
+@app.route('/images/calibrate', methods=['GET'])
+def calibrate():
+    print('[Info]\t\tCalibrating the following images')
+    imagens = procurar_imagens()
+    for img in imagens:
+        print("[Info]\t\t\t"+img)
+
+    corrigir_imagens(imagens, 'calibrada')
+    resp = {'message': 'Ok'
+            }
+    response_pickled = jsonpickle.encode(resp)
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+
+
+@app.route('/images/upload', methods=['POST'])
+def upload():
+    print("Received a request")
+    file_name = ''
+    if 'label' in request.headers:
+        file_name = request.headers.get('label')
+
+    nparr = np.fromstring(request.data, np.uint8)
+    # decode image
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    print("Saving the image " + file_name)
+
+    # TODO: recuperar nome da imagem
+    cv2.imwrite('./download/'+file_name, img)
+    # Visualizar imagem enviada
+    #cv2.imshow('URL2Image', img)
+    # cv2.waitKey()
+
+    print("Listing directory")
+    arquivos = procurar_imagens("./download")
+    for arquivo in arquivos:
+        print('\t\t- {0}'.format(arquivo))
+
+    # build a response dict to send back to client
+    response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0]),
+                'size': img.size,
+                'nbytes': img.nbytes,
+                }
+    # encode response using jsonpickle
+    print("Send response: " + str(response))
+    response_pickled = jsonpickle.encode(response)
+
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+
+
+# start flask app
+app.run(host="0.0.0.0", port=5000)
