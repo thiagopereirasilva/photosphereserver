beb879defb7312d7460d88835b9f3833607ddeea
Thiago Pereira da Silva <thiagosilva.inf@gmail.com>
2019-12-20 10:11:30 -0300

    Versao funcional da parte server

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, Response, url_for, send_file
+from flask import Flask, request, Response, url_for, send_file, send_from_directory, safe_join, abort
 from flask_pymongo import PyMongo
 from PIL import Image
 from zipfile import ZipFile
 import numpy as np
+import pymongo
 import sys
 import getopt
 import glob
@@ -12,11 +13,16 @@ import werkzeug
 import jsonpickle
 import cv2
 import base64
+import datetime
+import time
 
 # Initialize the Flask application
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/'
-mongo = PyMongo(app)
+app.config["CLIENT_IMAGES"] = "/home/thiago/Desktop/Workspace/PhotoSphere/download"
+
+# Initialize Mongo connector
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["photosphere"]
 
 
 def procurar_imagens(pasta_fonte='./download'):
@@ -25,7 +31,7 @@ def procurar_imagens(pasta_fonte='./download'):
 
     if arquivos:
         for arquivo in arquivos:
-            if (arquivo.endswith(".jpg") != True):
+            if (arquivo.endswith(".jpeg") != True):
                 arquivos.remove(arquivo)
     else:
         print('[Alerta]\tNenhum arquivo JPEG encontrado em "{0}"!'.format(
@@ -71,34 +77,11 @@ def corrigir_imagens(imagens, pasta_destino=''):
         img.save(nova_imagem, "jpeg", exif=exif_bytes)
 
 
-@app.route('/images/hdr', methods=['GET'])
-def calibrate():
-    uuid_code = '408dd860-b363-489f-8f53-b8fae89a312f'
-    luminance = '50'
-    error_msg = ''
-
-    if 'phone_UUID' in request.headers:
-        uuid_code = request.headers.get('phone_UUID')
-    else:
-        error_msg = "Cannot generate HDR. No UUID has been specified."
-
-    if 'luminance_factor' in request.headers:
-        luminance = request.headers.get('luminance_factor')
-    else:
-        error_msg = "Cannot generate HDR. No luminance value has been specified."
-
-    # If not luminance or uuid
-    # if error_msg:
-    #     print('[Error]\t\t' + error_msg)
-    #     resp = {'message': error_msg
-    #             }
-    #     return Response(response=jsonpickle.encode(resp), status=500, mimetype="application/json")
-
+@app.route('/images/hdr/<uuid_code>/<calibration>', methods=['GET'])
+def generate_hdr(uuid_code, calibration):
     print('[Info]\tCalibrating the following images')
     path = os.getcwd()
     path = path + '/download/' + uuid_code
-    # if (os.path.isdir(path) == False):
-    # throw exception
 
     imagens = procurar_imagens(path)
     images_path = ''
@@ -109,19 +92,16 @@ def calibrate():
 
     print('[Info]\tRunning hdrgen')
     # call hdrgen
+    # hdr_path = "/download/" + uuid_code + "/" + uuid_code + "_output.jpg"
     os.popen('./hdrgen/hdrgen -o ' + "/download/" + uuid_code +
-             "/" + uuid_code + "_output.jpg " + images_path)
+             "/" + uuid_code + "_output.jpg" + " " + images_path)
 
-    # send response
-    resp = {'message': 'Ok. HDR gerado'
-            }
-    response_pickled = jsonpickle.encode(resp)
-    return Response(response=response_pickled, status=200, mimetype="application/json")
+    return send_file("teste.jpg", mimetype="image/gif")
 
 
 @app.route('/images/upload', methods=['POST'])
 def upload():
-    print("Received a request")
+    print("[Info]\tReceived a request")
     file_name = ''
     uuid_code = ''
 
@@ -140,49 +120,92 @@ def upload():
     path = path + '/download/' + uuid_code
     if (os.path.isdir(path) == False):
         os.mkdir(path)
-    print("Saving image <" + file_name + "> belongs to ImageSet <" +
+    print("[Info]\t\tSaving image <" + file_name + "> belongs to ImageSet <" +
           uuid_code + "> \n on directory \n" + path)
 
     # cv2.imwrite(path +'/'+file_name, img)
     with open(path+'/'+file_name, 'wb') as f_output:
         f_output.write(array_img)
 
-    # build a response dict to send back to client
-    # response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0]),
-     #           'size': img.size,
-      #          'nbytes': img.nbytes,
-        #         }
-    response = {'message': 'cabeca de tecao'}
-    print("Send response: " + str(response))
+    response = {'message': 'created'}
+    print("[Info]\t\tSend response: " + str(response))
     response_pickled = jsonpickle.encode(response)
+    return Response(response=response_pickled, status=201, mimetype="application/json")
+
+
+# docker run -d -p 27017:27017 -p 28017:28017 mongo
+@app.route('/imageset', methods=['POST'])
+def create_imageset():
+    print("[Info]\tSalving imageSet")
+    content = request.json
+    uid = content['uuid']
+
+    # getting images paths
+    path = os.getcwd()
+    path = path + '/download/' + uid
+    images_path = procurar_imagens(path)
+    images_path_str = []
+    for img in images_path:
+        # images_path_str.append(img)
+        arr = img.split("/")
+        images_path_str.append(arr[len(arr)-1])
+
+    # getting actual time (long format)
+    actual_time = int(round(time.time()*1000))
+
+    doc = {'_id': uid, 'uuid': uid, 'label': content['label'],
+           'description': content['description'],
+           'author': content['author'], 'created_date': actual_time}
+    doc['images_paths'] = images_path_str
+
+    customers = db["imageset"]
+    ids = customers.insert(doc)
+    print('[Info]\t\tImageset ' + str(ids) + ' created')
+    resp = {'message': 'Imageset ' + str(ids)+' created'}
+    response_pickled = jsonpickle.encode(resp)
+
+    return Response(response=response_pickled, status=201, mimetype="application/json")
+
+
+@app.route('/imageset', methods=['GET'])
+def getAllImageSet():
+    print("[Info]\t\tGetting all imageSet")
+    all_imageset = []
+    customers = db["imageset"]
+    for x in customers.find():
+        dd = x['created_date']
+        x['created_date'] = str(dd)
+        pathService = []
+        for path in x['images_paths']:
+            # print(path)
+            path = "http://10.7.128.18:5000/images/" + x['_id'] + "/" + path
+            # path = url_for('/images/' + x['_id'] + "/" + path)
+            # path = url_for('uploadTest')
+            pathService.append(path)
+
+        x['images_paths'] = pathService
+
+        all_imageset.append(x)
+
+    # print(all_imageset)
+    response_pickled = jsonpickle.encode(all_imageset)
     return Response(response=response_pickled, status=200, mimetype="application/json")
 
 
-@app.route('/images/get_hdr', methods=['GET'])
-def uploadTest():
-    print("Testing image send")
-    filename = 'teste.jpg'
-    return send_file(filename, mimetype='image/jpeg', attachment_filename="ronaldo")
+@app.route('/images/<uuid_code>/<image>', methods=['GET'])
+def uploadTest(uuid_code, image):
+    print("[Info]\t\tSending image")
+    image_path = "download/"+uuid_code+"/"+image
+    return send_file(image_path, mimetype='image/jpg')
 
 
-@app.route('/images/save', methods=['GET'])
-def save():
-    print("salvando no banco")
-    img = cv2.imread("teste.jpg")
-    mongo.save_file("arquivo_thiago", img)
-    mongo.db.users.insert({'username': 'Thiago', 'arquivo': img})
-
-    # content_type = 'image/jpeg'
-    # image_name = 'teste.jpg'
-    # headers = {'content-type': content_type,
-    #            'phone_description': 'Flamengo vai levar peia!'
-    #            }
-    # img = cv2.imread(image_name)
-    # _, img_encoded = cv2.imencode('.jpg', img)
-
-    # return Response(data=img_encoded, status=200, mimetype="image/jpeg")
+# @app.route("/image/<image_name>", methods=['GET'])
+# def get_image(image_name):
+#     try:
+#         return send_from_directory(app.config["CLIENT_IMAGES"], filename=image_name, as_attachment=True)
+#     except:
+#         abort(404)
 
 
 # start flask app
-
 app.run(host="0.0.0.0", port=5000)
